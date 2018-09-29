2064e894c16b5c77b0e6e798a2e1ab7d263b3f45
mattschultz1337 <mschult7@binghamton.edu>
2018-09-29 13:03:59 -0400

    initial commit

--- Voter.cpp
+++ Voter.cpp
@@ -10,8 +10,34 @@ void cmd_view(string& lastname,string& firstname, int& age, int& stnum, string& 
 void cmd_report(string lastname, float amtdonated);
 void cmd_donate(float& amtdonated);
 
-class Voter;
-class VoterDB;
+class Voter {
+   private:
+     string lastname;
+     string firstname;
+     string userid;
+     string password;
+     int age;
+     int stnum;
+     string street;
+     string town;
+     string zip;
+     float amtdonated;
+   public:
+     int main(int argc, char *argv[]);
+     Voter(){
+       string lastname="[EMPTY]";
+       string firstname="[EMPTY]";
+       userid="userid1";
+       password="pass-word2";
+       int age=0;
+       int stnum=0;
+       string street="[EMPTY]";
+       string town="[EMPTY]";
+       string zip="00000";
+       float amtdonated=0;
+     }
+
+};
 int main(int argc, char *argv[]) {
    cout << "Enter a command(Login,New,Report,Save,Load, or Quit)";
    cout << ">: ";
